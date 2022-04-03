367c9d06ad0fd1968a43c67d3a7a02831fcfa1ac
Zeena Al-Katib <zeena.alkatib@gmail.com>
2022-04-03 03:54:35 -0400

    updated styles

--- src/components/NotionPage.js
+++ src/components/NotionPage.js
@@ -2,6 +2,20 @@ import React, { useEffect } from "react";
 import { loadNotionContent } from "../utils/utils";
 import { NotionRenderer } from "react-notion";
 import "react-notion/src/styles.css";
+import styled from "styled-components";
+
+const Content = styled.div`
+  .notion {
+    color: var(--color-foreground) !important;
+    a {
+      color: var(--color-foreground) !important;
+    }
+  }
+  .notion-code {
+    background-color: var(--color-code) !important;
+  }
+  font-size: 300px;
+`;
 
 const NotionPage = ({ pageId }) => {
   const [loading, setLoading] = React.useState(null);
@@ -9,7 +23,7 @@ const NotionPage = ({ pageId }) => {
 
   const loadPage = React.useCallback(() => {
     setLoading(true);
-    loadNotionContent('page', pageId)
+    loadNotionContent("page", pageId)
       .then((res) => {
         console.log(res);
         setPage(res.data);
@@ -23,9 +37,7 @@ const NotionPage = ({ pageId }) => {
     if (loading === null) loadPage();
   }, [loading, loadPage]);
 
-  return (
-    <div className="content">{page && <NotionRenderer blockMap={page} />}</div>
-  );
+  return <Content>{page && <NotionRenderer blockMap={page} />}</Content>;
 };
 
 export default NotionPage;
