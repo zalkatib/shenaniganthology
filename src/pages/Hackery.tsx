1fc74490eb6e07054b9806fd567e3d662cc24eac
Zeena Al-Katib <zeena.alkatib@gmail.com>
2022-04-13 03:14:15 -0400

    loading state

--- src/pages/Hackery.tsx
+++ src/pages/Hackery.tsx
@@ -7,24 +7,23 @@ const Hackery: React.FC = () => {
   const tableId =
     "76e7706a62cd4e33981650a9bfea1264?v=6ea897791a8742229b30f3bfa4d4e5d7";
 
-  const [loadingTable, setLoadingTable] = React.useState(null);
+  const [tableLoaded, setTableLoaded] = React.useState(null);
   const [table, setTable] = React.useState(null);
 
   const loadTable = React.useCallback(() => {
-    setLoadingTable(true);
+    setTableLoaded(false);
     loadNotionContent("table", tableId)
       .then((res) => {
-        console.log(res);
         setTable(res.data);
       })
       .finally(() => {
-        setLoadingTable(false);
+        setTableLoaded(true);
       });
   }, []);
 
   React.useEffect(() => {
-    if (loadingTable === null) loadTable();
-  }, [table, loadingTable, loadTable]);
+    if (tableLoaded === null) loadTable();
+  }, [table, tableLoaded, loadTable]);
 
   function pageList() {
     return (
@@ -39,30 +38,32 @@ const Hackery: React.FC = () => {
   }
 
   return (
-    <div>
-      <h1
-        style={{
-          borderBottom: "var(--color-hover) 1px dashed",
-          margin: 0,
-          padding: "10px",
-        }}
-      >
-        failing early and often
-      </h1>
-      <div className={styles.hackeryContent}>
-        <div style={{ borderRight: "var(--color-hover) 1px dashed" }}>
-          <h2 style={{ marginBottom: 0, paddingRight: "50px" }}>
-            in writing form. but with absolutely no helpful pagination or
-            anything
-          </h2>
-          {table && pageList()}
-        </div>
-        <div>
-          <h2>in project form. if there were projects to put here</h2>there
-          aren't though
+    tableLoaded && (
+      <div>
+        <h1
+          style={{
+            borderBottom: "var(--color-hover) 1px dashed",
+            margin: 0,
+            padding: "10px",
+          }}
+        >
+          failing early and often
+        </h1>
+        <div className={styles.hackeryContent}>
+          <div style={{ borderRight: "var(--color-hover) 1px dashed" }}>
+            <h2 style={{ marginBottom: 0, paddingRight: "50px" }}>
+              in writing form. but with absolutely no helpful pagination or
+              anything
+            </h2>
+            {table && pageList()}
+          </div>
+          <div>
+            <h2>in project form. if there were projects to put here</h2>there
+            aren't though
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
